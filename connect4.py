0ac606970e3f903070aa8f6b525ce018fbdd2817
Aishwarya2345 <aishu1994.ravi@gmail.com>
2020-04-01 21:35:15 +0100

    added dev mode to remove ui for bot vs bot game

--- connect4.py
+++ connect4.py
@@ -9,6 +9,10 @@ from bots import *
 board = None
 gb = None
 
+# dev statement to turn of the UI when having a bot vs bot match
+# turning UI off in this case helps improve the performance of the bots.
+graphics = True
+
 PLAYER_COLOUR = [GBoard.RED, GBoard.YELLOW]
 
 game_over = False
@@ -16,8 +20,10 @@ turn = random.randint(Board.PLAYER1_PIECE, Board.PLAYER2_PIECE)
 
 def next_turn():
 	global turn
+	print("\nPlayer " + str(turn) + "'s Turn\n")
 	board.print_board()
-	gb.draw_gboard(board)
+	if graphics:
+		gb.draw_gboard(board)
 
 	if turn == board.PLAYER1_PIECE:
 		turn = board.PLAYER2_PIECE
@@ -26,22 +32,29 @@ def next_turn():
 
 def check_win(piece):
 	if board.winning_move(piece):
-		gb.write_on_board("Player" + str(piece) + " wins!!", PLAYER_COLOUR[piece-1], (40, 10))
+		if graphics:
+			gb.write_on_board("Player" + str(piece) + " wins!!", PLAYER_COLOUR[piece-1], (40, 10))
+			gb.update_gboard()
+		print("\nPlayer" + str(piece) + " wins!!")
 		return True
 	if board.check_draw():
-		gb.write_on_board("Draw!!", gb.BLUE, (240, 10))
+		if graphics:
+			gb.write_on_board("Draw!!", gb.BLUE, (240, 10))
+			gb.update_gboard()
+		print("\nDraw!!")
 		return True
 	return False
 
 def connect4(p1, p2):
-	global game_over, board, gb
+	global game_over, board, gb, graphics
 
 	board = Board()
-	gb = GBoard(board)
-
 	board.print_board()
-	gb.draw_gboard(board)
-	gb.update_gboard()
+
+	if graphics:
+		gb = GBoard(board)
+		gb.draw_gboard(board)
+		gb.update_gboard()
 
 	while not game_over:
 		# Player1's Input
@@ -50,8 +63,8 @@ def connect4(p1, p2):
 
 			if board.is_valid_location(col):
 				board.drop_piece(col, board.PLAYER1_PIECE)
-				game_over = check_win(board.PLAYER1_PIECE)
 				next_turn()
+				game_over = check_win(board.PLAYER1_PIECE)
 
 		# Player2's Input
 		if turn == board.PLAYER2_PIECE and not game_over:
@@ -59,8 +72,8 @@ def connect4(p1, p2):
 
 			if board.is_valid_location(col):
 				board.drop_piece(col, board.PLAYER2_PIECE)
-				game_over = check_win(board.PLAYER2_PIECE)
 				next_turn()
+				game_over = check_win(board.PLAYER2_PIECE)
 
 		if game_over:
 			pygame.time.wait(3000)
