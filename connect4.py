be1568f986042e5fcdc900b1448f91b393aa8e22
Mukesh A <amukesh.mk@gmail.com>
2020-04-05 12:26:19 +0100

    added counter and time measures to player moves

--- connect4.py
+++ connect4.py
@@ -1,15 +1,15 @@
 import numpy as np
-
-#pygame version number and welcome message hidden.
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-
 import pygame
 import sys
 import math
 import random
+import time
 from board import *
 from bots import *
+
+#pygame version number and welcome message hidden.
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 board = None
 gb = None
@@ -40,7 +40,7 @@ def check_win(piece):
 		if graphics:
 			gb.write_on_board("PLAYER " + str(piece) + " WINS!", PLAYER_COLOUR[piece - 1], 350, 50, 70, True)
 			gb.update_gboard()
-		print("\PLAYER " + str(piece) + "WINS!")
+		print("\nPLAYER " + str(piece) + " WINS!")
 		return True
 	
 	if board.check_draw():
@@ -64,27 +64,48 @@ def connect4(p1, p2, ui=True):
 		gb.draw_gboard(board)
 		gb.update_gboard()
 
+	time_p1 = time_p2 = 0
+	moves_count_p1 = moves_count_p2 = 0
+
 	while not game_over:
 		# Player1's Input
+		start = time.perf_counter()
 		if turn == board.PLAYER1_PIECE and not game_over:
 			col = p1.get_move(board)
 
 			if board.is_valid_location(col):
 				board.drop_piece(col, board.PLAYER1_PIECE)
+				moves_count_p1 += 1
 				next_turn()
 				game_over = check_win(board.PLAYER1_PIECE)
+		end = time.perf_counter()
+
+		time_p1 += (end - start)
 
 		# Player2's Input
+		start = time.perf_counter()
 		if turn == board.PLAYER2_PIECE and not game_over:
 			col = p2.get_move(board)
 
 			if board.is_valid_location(col):
 				board.drop_piece(col, board.PLAYER2_PIECE)
+				moves_count_p2 += 1
 				next_turn()
 				game_over = check_win(board.PLAYER2_PIECE)
+		end = time.perf_counter()
+
+		time_p2 += (end - start)
 
 		if game_over:
 			pygame.time.wait(1000)
+
+			print("\nPlayer 1")
+			print("TIME: " + "{:.2f}".format(round(time_p1, 2)) + " seconds")
+			print("MOVES: "+ str(moves_count_p1))
+			print("\nPlayer 2")
+			print("TIME: " + "{:.2f}".format(round(time_p2, 2)) + " seconds")
+			print("MOVES: "+ str(moves_count_p2))
+
 			sys.exit()
 
 if __name__ == "__main__":
